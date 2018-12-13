c4582f31bee7ac1a501731cc4569f8cb9852dc1d
chomarlay <chomarlay.aung@gmail.com>
2018-12-14 06:01:53 +1100

    more routing

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,11 @@
 import React from 'react';
-
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post" onClick={props.clicked}>
+const post = (props) => (<article className="Post" onClick={props.clicked}>
         <h1>{props.title}</h1>
         <div className="Info">
             <div className="Author">{props.author}</div>
         </div>
-    </article>
-);
+    </article>)
 
 export default post;
