f0fd2e84f40a08b4d856663bc406c1c7cf0e2c07
chomarlay <chomarlay.aung@gmail.com>
2018-12-21 18:18:43 +1100

    nested routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import  { Route, NavLink, Switch } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -16,7 +15,7 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink 
-                            to="/" exact
+                            to="/posts" exact
                             activeClassName="active"
                             >Posts</NavLink></li>
                             <li><NavLink to={{pathname: '/new-post',
@@ -29,9 +28,9 @@ class Blog extends Component {
                 {/*<Route path='/' exact render ={()=> <h1>Home</h1>} />
                 <Route path='/'  render ={()=> <h1>Home 2</h1>} />*/}
                 <Switch>
-                    <Route path="/" exact component={Posts}/>
                     <Route path="/new-post" component={NewPost}/>
-                    <Route path="/:id"  exact component={FullPost}/>
+                    <Route path="/posts" component={Posts}/>
+
                 </Switch>
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import {Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
 
 import './Posts.css';
 
@@ -29,8 +31,9 @@ class Posts extends Component {
 
     postSelectedHandler (id){
         // programatically load the page instead of Link
-        // this.props.history.push({pathname: '/' + id});
-        // this.props.history.push('/' + id);
+        // this.props.history.push({pathname: '/posts/' + id});
+        this.props.history.push('/posts/' + id);
+        console.log(this.props.match.url);
        }
 render() {
     let posts = <p style= {{textAlign:'center'}}>Something went wrong!!</p>
@@ -38,21 +41,24 @@ render() {
         posts = this.state.posts.map(
             post => {
                 return (
-                <Link to={'/'+post.id} key={post.id}>
-                <Post 
-                title={post.title} 
-                author={post.author}
-                // clicked={() => this.postSelectedHandler(post.id)} 
-                />
-                </Link>
+                    <Link to={'/posts/' + post.id} key={post.id}>
+                        <Post
+                            title={post.title}
+                            author={post.author}
+                        // clicked={() => this.postSelectedHandler(post.id)} 
+                        />
+                    </Link>
                 )
                 ;
             });
     }
     return (
-        <section className="Posts">
-            {posts}
-        </section>
+        <div>
+            <section className="Posts">
+                {posts}
+            </section>
+            <Route path={this.props.match.url + '/:id'} exact component={FullPost}/>
+        </div>
     )
 }
 }
