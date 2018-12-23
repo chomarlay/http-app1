b57500a4a796d3d55789b48908165cc9a89cd344
chomarlay <chomarlay.aung@gmail.com>
2018-12-24 01:02:07 +1100

    lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import  { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 import './Blog.css';
+
+const NewPost = React.lazy(()=> import('./NewPost/NewPost'));
 
 class Blog extends Component {
     state = {
@@ -30,8 +32,15 @@ class Blog extends Component {
                 {/*<Route path='/' exact render ={()=> <h1>Home</h1>} />
                 <Route path='/'  render ={()=> <h1>Home 2</h1>} />*/}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost}/>:null} 
+                    {/* {this.state.auth ? <Route path="/new-post" component={NewPost}/>:null}  */}
                     
+                    {this.state.auth ? <Route path="/new-post"
+                        render={() => (
+                            <Suspense fallback={<div>Loding ...</div>}>
+                                <NewPost />
+                            </Suspense>
+                        )} /> : null} 
+
                     <Route path="/posts" component={Posts}/>
                     {/* <Route render={()=><h1>Not found</h1>}/> */}
                     <Redirect from="/" to="/posts" />
