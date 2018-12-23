9279864ea7e779b4cfed893fce508bfae3bf3863
chomarlay <chomarlay.aung@gmail.com>
2018-12-24 00:13:10 +1100

    route guard

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,8 +5,10 @@ import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-
-
+    state = {
+        auth: true
+    }
+    
     render () {
 
         return (
@@ -28,7 +30,8 @@ class Blog extends Component {
                 {/*<Route path='/' exact render ={()=> <h1>Home</h1>} />
                 <Route path='/'  render ={()=> <h1>Home 2</h1>} />*/}
                 <Switch>
-                    <Route path="/new-post" component={NewPost}/>
+                    {this.state.auth ? <Route path="/new-post" component={NewPost}/>:null} 
+                    
                     <Route path="/posts" component={Posts}/>
                     <Redirect from="/" to="/posts" />
 
